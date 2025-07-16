37448bd9948b4bbb52aa8d55e9d08328705f39dd
N3agu <andreei1465@yahoo.com>
2025-07-16 17:00:19 +0000

    Implement methods to get proc name by pid and convert file operation opcodes to strings

--- HoundSentry/ProcessUtils.cpp
+++ HoundSentry/ProcessUtils.cpp
@@ -1,4 +1,6 @@
-#include <windows.h>
+#include "ProcessUtils.h"
+#include <TlHelp32.h>
+#include <iostream>
 
 bool IsProcessElevated() {
     HANDLE hToken = nullptr;
@@ -20,4 +22,56 @@ bool IsProcessElevated() {
     }
 
     return isElevated;
+}
+
+std::wstring GetProcessNameByPid(DWORD processId) {
+    if (processId == 0) return L"System Idle Process";
+    if (processId == 4) return L"System";
+
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnap == INVALID_HANDLE_VALUE) {
+        return L"N/A";
+    }
+
+    PROCESSENTRY32W pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+
+    if (Process32FirstW(hSnap, &pe32)) {
+        do {
+            if (pe32.th32ProcessID == processId) {
+                CloseHandle(hSnap);
+                return std::wstring(pe32.szExeFile);
+            }
+        } while (Process32NextW(hSnap, &pe32));
+    }
+
+    CloseHandle(hSnap);
+    return L"N/A";
+}
+
+std::string GetFileOperationName(UCHAR opcode) {
+    switch (opcode) {
+        case 10: return "FileRead";
+        case 11: return "FileWrite";
+        case 32: return "FileCreate";
+        case 35: return "FileDelete";
+        case 36: return "FileRename";
+        case 37: return "FileQueryInformation";
+
+        default: return "Other (" + std::to_string(opcode) + ")";
+    }
+}
+
+std::string GetRegistryOperationName(UCHAR opcode) {
+    switch (opcode) {
+        case 10: return "RegCreateKey";
+        case 11: return "RegOpenKey";
+        case 12: return "RegDeleteKey";
+        case 13: return "RegQueryKey";
+        case 14: return "RegSetValue";
+        case 15: return "RegDeleteValue";
+        case 16: return "RegQueryValue";
+
+        default: return "Other (" + std::to_string(opcode) + ")";
+    }
 }
--- HoundSentry/ProcessUtils.h
+++ HoundSentry/ProcessUtils.h
@@ -1,3 +1,11 @@
 #pragma once
+#include <string>
+#include <windows.h>
 
 bool IsProcessElevated();
+
+std::wstring GetProcessNameByPid(DWORD processId);
+
+std::string GetFileOperationName(UCHAR opcode);
+
+std::string GetRegistryOperationName(UCHAR opcode);
